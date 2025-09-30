c23c3cf9221191f439e2c2d91fa93163df958e26
Andrej Kabachnik <andrej.kabachnik@accenture.com>
2025-09-30 08:23:51 +0200

    FIX installer error on case sensitive file systesm

--- Facades/HttpBarcodeFacade.php
+++ Facades/HttpBarcodeFacade.php
@@ -47,7 +47,7 @@ class HttpBarcodeFacade extends AbstractHttpFacade
         require_once $this->getWorkbench()->getInstallationPath()
             . DIRECTORY_SEPARATOR . 'vendor'
             . DIRECTORY_SEPARATOR . 'exface'
-            . DIRECTORY_SEPARATOR . 'barcodeScanner'
+            . DIRECTORY_SEPARATOR . 'BarcodeScanner'
             . DIRECTORY_SEPARATOR . 'CommonLogic'
             . DIRECTORY_SEPARATOR . 'barcode.php';
     }
